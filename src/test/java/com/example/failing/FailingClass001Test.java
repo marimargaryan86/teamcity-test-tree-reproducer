44dd6b0c90709e5c8c0d3fd805ff14ee610c3083
Mari Margaryan <mari.margaryan86@gmail.com>
2025-12-25 20:53:09 +0400

    Increase FailingClass001Test to 102 failing tests

--- src/test/java/com/example/failing/FailingClass001Test.java
+++ src/test/java/com/example/failing/FailingClass001Test.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
- * Intentionally failing test class with 20 tests for TeamCity UI testing.
+ * Intentionally failing test class with 102 tests for TeamCity UI testing.
  */
 public class FailingClass001Test {
 
@@ -106,5 +106,415 @@ public class FailingClass001Test {
     @Test
     void failingTest020_class001() {
         fail("Intentional failure: FailingClass001_Test020");
+    }
+
+    @Test
+    void failingTest021_class001() {
+        fail("Intentional failure: FailingClass001_Test021");
+    }
+
+    @Test
+    void failingTest022_class001() {
+        fail("Intentional failure: FailingClass001_Test022");
+    }
+
+    @Test
+    void failingTest023_class001() {
+        fail("Intentional failure: FailingClass001_Test023");
+    }
+
+    @Test
+    void failingTest024_class001() {
+        fail("Intentional failure: FailingClass001_Test024");
+    }
+
+    @Test
+    void failingTest025_class001() {
+        fail("Intentional failure: FailingClass001_Test025");
+    }
+
+    @Test
+    void failingTest026_class001() {
+        fail("Intentional failure: FailingClass001_Test026");
+    }
+
+    @Test
+    void failingTest027_class001() {
+        fail("Intentional failure: FailingClass001_Test027");
+    }
+
+    @Test
+    void failingTest028_class001() {
+        fail("Intentional failure: FailingClass001_Test028");
+    }
+
+    @Test
+    void failingTest029_class001() {
+        fail("Intentional failure: FailingClass001_Test029");
+    }
+
+    @Test
+    void failingTest030_class001() {
+        fail("Intentional failure: FailingClass001_Test030");
+    }
+
+    @Test
+    void failingTest031_class001() {
+        fail("Intentional failure: FailingClass001_Test031");
+    }
+
+    @Test
+    void failingTest032_class001() {
+        fail("Intentional failure: FailingClass001_Test032");
+    }
+
+    @Test
+    void failingTest033_class001() {
+        fail("Intentional failure: FailingClass001_Test033");
+    }
+
+    @Test
+    void failingTest034_class001() {
+        fail("Intentional failure: FailingClass001_Test034");
+    }
+
+    @Test
+    void failingTest035_class001() {
+        fail("Intentional failure: FailingClass001_Test035");
+    }
+
+    @Test
+    void failingTest036_class001() {
+        fail("Intentional failure: FailingClass001_Test036");
+    }
+
+    @Test
+    void failingTest037_class001() {
+        fail("Intentional failure: FailingClass001_Test037");
+    }
+
+    @Test
+    void failingTest038_class001() {
+        fail("Intentional failure: FailingClass001_Test038");
+    }
+
+    @Test
+    void failingTest039_class001() {
+        fail("Intentional failure: FailingClass001_Test039");
+    }
+
+    @Test
+    void failingTest040_class001() {
+        fail("Intentional failure: FailingClass001_Test040");
+    }
+
+    @Test
+    void failingTest041_class001() {
+        fail("Intentional failure: FailingClass001_Test041");
+    }
+
+    @Test
+    void failingTest042_class001() {
+        fail("Intentional failure: FailingClass001_Test042");
+    }
+
+    @Test
+    void failingTest043_class001() {
+        fail("Intentional failure: FailingClass001_Test043");
+    }
+
+    @Test
+    void failingTest044_class001() {
+        fail("Intentional failure: FailingClass001_Test044");
+    }
+
+    @Test
+    void failingTest045_class001() {
+        fail("Intentional failure: FailingClass001_Test045");
+    }
+
+    @Test
+    void failingTest046_class001() {
+        fail("Intentional failure: FailingClass001_Test046");
+    }
+
+    @Test
+    void failingTest047_class001() {
+        fail("Intentional failure: FailingClass001_Test047");
+    }
+
+    @Test
+    void failingTest048_class001() {
+        fail("Intentional failure: FailingClass001_Test048");
+    }
+
+    @Test
+    void failingTest049_class001() {
+        fail("Intentional failure: FailingClass001_Test049");
+    }
+
+    @Test
+    void failingTest050_class001() {
+        fail("Intentional failure: FailingClass001_Test050");
+    }
+
+    @Test
+    void failingTest051_class001() {
+        fail("Intentional failure: FailingClass001_Test051");
+    }
+
+    @Test
+    void failingTest052_class001() {
+        fail("Intentional failure: FailingClass001_Test052");
+    }
+
+    @Test
+    void failingTest053_class001() {
+        fail("Intentional failure: FailingClass001_Test053");
+    }
+
+    @Test
+    void failingTest054_class001() {
+        fail("Intentional failure: FailingClass001_Test054");
+    }
+
+    @Test
+    void failingTest055_class001() {
+        fail("Intentional failure: FailingClass001_Test055");
+    }
+
+    @Test
+    void failingTest056_class001() {
+        fail("Intentional failure: FailingClass001_Test056");
+    }
+
+    @Test
+    void failingTest057_class001() {
+        fail("Intentional failure: FailingClass001_Test057");
+    }
+
+    @Test
+    void failingTest058_class001() {
+        fail("Intentional failure: FailingClass001_Test058");
+    }
+
+    @Test
+    void failingTest059_class001() {
+        fail("Intentional failure: FailingClass001_Test059");
+    }
+
+    @Test
+    void failingTest060_class001() {
+        fail("Intentional failure: FailingClass001_Test060");
+    }
+
+    @Test
+    void failingTest061_class001() {
+        fail("Intentional failure: FailingClass001_Test061");
+    }
+
+    @Test
+    void failingTest062_class001() {
+        fail("Intentional failure: FailingClass001_Test062");
+    }
+
+    @Test
+    void failingTest063_class001() {
+        fail("Intentional failure: FailingClass001_Test063");
+    }
+
+    @Test
+    void failingTest064_class001() {
+        fail("Intentional failure: FailingClass001_Test064");
+    }
+
+    @Test
+    void failingTest065_class001() {
+        fail("Intentional failure: FailingClass001_Test065");
+    }
+
+    @Test
+    void failingTest066_class001() {
+        fail("Intentional failure: FailingClass001_Test066");
+    }
+
+    @Test
+    void failingTest067_class001() {
+        fail("Intentional failure: FailingClass001_Test067");
+    }
+
+    @Test
+    void failingTest068_class001() {
+        fail("Intentional failure: FailingClass001_Test068");
+    }
+
+    @Test
+    void failingTest069_class001() {
+        fail("Intentional failure: FailingClass001_Test069");
+    }
+
+    @Test
+    void failingTest070_class001() {
+        fail("Intentional failure: FailingClass001_Test070");
+    }
+
+    @Test
+    void failingTest071_class001() {
+        fail("Intentional failure: FailingClass001_Test071");
+    }
+
+    @Test
+    void failingTest072_class001() {
+        fail("Intentional failure: FailingClass001_Test072");
+    }
+
+    @Test
+    void failingTest073_class001() {
+        fail("Intentional failure: FailingClass001_Test073");
+    }
+
+    @Test
+    void failingTest074_class001() {
+        fail("Intentional failure: FailingClass001_Test074");
+    }
+
+    @Test
+    void failingTest075_class001() {
+        fail("Intentional failure: FailingClass001_Test075");
+    }
+
+    @Test
+    void failingTest076_class001() {
+        fail("Intentional failure: FailingClass001_Test076");
+    }
+
+    @Test
+    void failingTest077_class001() {
+        fail("Intentional failure: FailingClass001_Test077");
+    }
+
+    @Test
+    void failingTest078_class001() {
+        fail("Intentional failure: FailingClass001_Test078");
+    }
+
+    @Test
+    void failingTest079_class001() {
+        fail("Intentional failure: FailingClass001_Test079");
+    }
+
+    @Test
+    void failingTest080_class001() {
+        fail("Intentional failure: FailingClass001_Test080");
+    }
+
+    @Test
+    void failingTest081_class001() {
+        fail("Intentional failure: FailingClass001_Test081");
+    }
+
+    @Test
+    void failingTest082_class001() {
+        fail("Intentional failure: FailingClass001_Test082");
+    }
+
+    @Test
+    void failingTest083_class001() {
+        fail("Intentional failure: FailingClass001_Test083");
+    }
+
+    @Test
+    void failingTest084_class001() {
+        fail("Intentional failure: FailingClass001_Test084");
+    }
+
+    @Test
+    void failingTest085_class001() {
+        fail("Intentional failure: FailingClass001_Test085");
+    }
+
+    @Test
+    void failingTest086_class001() {
+        fail("Intentional failure: FailingClass001_Test086");
+    }
+
+    @Test
+    void failingTest087_class001() {
+        fail("Intentional failure: FailingClass001_Test087");
+    }
+
+    @Test
+    void failingTest088_class001() {
+        fail("Intentional failure: FailingClass001_Test088");
+    }
+
+    @Test
+    void failingTest089_class001() {
+        fail("Intentional failure: FailingClass001_Test089");
+    }
+
+    @Test
+    void failingTest090_class001() {
+        fail("Intentional failure: FailingClass001_Test090");
+    }
+
+    @Test
+    void failingTest091_class001() {
+        fail("Intentional failure: FailingClass001_Test091");
+    }
+
+    @Test
+    void failingTest092_class001() {
+        fail("Intentional failure: FailingClass001_Test092");
+    }
+
+    @Test
+    void failingTest093_class001() {
+        fail("Intentional failure: FailingClass001_Test093");
+    }
+
+    @Test
+    void failingTest094_class001() {
+        fail("Intentional failure: FailingClass001_Test094");
+    }
+
+    @Test
+    void failingTest095_class001() {
+        fail("Intentional failure: FailingClass001_Test095");
+    }
+
+    @Test
+    void failingTest096_class001() {
+        fail("Intentional failure: FailingClass001_Test096");
+    }
+
+    @Test
+    void failingTest097_class001() {
+        fail("Intentional failure: FailingClass001_Test097");
+    }
+
+    @Test
+    void failingTest098_class001() {
+        fail("Intentional failure: FailingClass001_Test098");
+    }
+
+    @Test
+    void failingTest099_class001() {
+        fail("Intentional failure: FailingClass001_Test099");
+    }
+
+    @Test
+    void failingTest100_class001() {
+        fail("Intentional failure: FailingClass001_Test100");
+    }
+
+    @Test
+    void failingTest101_class001() {
+        fail("Intentional failure: FailingClass001_Test101");
+    }
+
+    @Test
+    void failingTest102_class001() {
+        fail("Intentional failure: FailingClass001_Test102");
     }
 }
