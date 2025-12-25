9bab9e732e5ad1a838e20fa6c4bf3b2b6d1ec694
Mari Margaryan <mari.margaryan86@gmail.com>
2025-12-25 20:47:07 +0400

    Increase FailingClass001Test to 20 failing tests for Show All button testing

--- src/test/java/com/example/failing/FailingClass001Test.java
+++ src/test/java/com/example/failing/FailingClass001Test.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
- * Intentionally failing test class for TeamCity UI testing.
+ * Intentionally failing test class with 20 tests for TeamCity UI testing.
  */
 public class FailingClass001Test {
 
@@ -56,5 +56,55 @@ public class FailingClass001Test {
     @Test
     void failingTest010_class001() {
         fail("Intentional failure: FailingClass001_Test010");
+    }
+
+    @Test
+    void failingTest011_class001() {
+        fail("Intentional failure: FailingClass001_Test011");
+    }
+
+    @Test
+    void failingTest012_class001() {
+        fail("Intentional failure: FailingClass001_Test012");
+    }
+
+    @Test
+    void failingTest013_class001() {
+        fail("Intentional failure: FailingClass001_Test013");
+    }
+
+    @Test
+    void failingTest014_class001() {
+        fail("Intentional failure: FailingClass001_Test014");
+    }
+
+    @Test
+    void failingTest015_class001() {
+        fail("Intentional failure: FailingClass001_Test015");
+    }
+
+    @Test
+    void failingTest016_class001() {
+        fail("Intentional failure: FailingClass001_Test016");
+    }
+
+    @Test
+    void failingTest017_class001() {
+        fail("Intentional failure: FailingClass001_Test017");
+    }
+
+    @Test
+    void failingTest018_class001() {
+        fail("Intentional failure: FailingClass001_Test018");
+    }
+
+    @Test
+    void failingTest019_class001() {
+        fail("Intentional failure: FailingClass001_Test019");
+    }
+
+    @Test
+    void failingTest020_class001() {
+        fail("Intentional failure: FailingClass001_Test020");
     }
 }
