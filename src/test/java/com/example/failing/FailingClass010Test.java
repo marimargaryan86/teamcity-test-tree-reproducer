832306ff698b353cb047993b145762abf6763e1c
Mari Margaryan <mari.margaryan86@gmail.com>
2025-12-26 15:49:27 +0400

    Update FailingClass010Test to 22 failing tests

--- src/test/java/com/example/failing/FailingClass010Test.java
+++ src/test/java/com/example/failing/FailingClass010Test.java
@@ -4,57 +4,117 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
- * Intentionally failing test class for TeamCity UI testing.
+ * Intentionally failing test class with 22 tests for TeamCity UI testing.
  */
 public class FailingClass010Test {
 
     @Test
-    void failingTest001_class010() {
-        fail("Intentional failure: FailingClass010_Test001");
+    void failingTest01_class010() {
+        fail("Intentional failure: FailingClass010_Test01");
     }
 
     @Test
-    void failingTest002_class010() {
-        fail("Intentional failure: FailingClass010_Test002");
+    void failingTest02_class010() {
+        fail("Intentional failure: FailingClass010_Test02");
     }
 
     @Test
-    void failingTest003_class010() {
-        fail("Intentional failure: FailingClass010_Test003");
+    void failingTest03_class010() {
+        fail("Intentional failure: FailingClass010_Test03");
     }
 
     @Test
-    void failingTest004_class010() {
-        fail("Intentional failure: FailingClass010_Test004");
+    void failingTest04_class010() {
+        fail("Intentional failure: FailingClass010_Test04");
     }
 
     @Test
-    void failingTest005_class010() {
-        fail("Intentional failure: FailingClass010_Test005");
+    void failingTest05_class010() {
+        fail("Intentional failure: FailingClass010_Test05");
     }
 
     @Test
-    void failingTest006_class010() {
-        fail("Intentional failure: FailingClass010_Test006");
+    void failingTest06_class010() {
+        fail("Intentional failure: FailingClass010_Test06");
     }
 
     @Test
-    void failingTest007_class010() {
-        fail("Intentional failure: FailingClass010_Test007");
+    void failingTest07_class010() {
+        fail("Intentional failure: FailingClass010_Test07");
     }
 
     @Test
-    void failingTest008_class010() {
-        fail("Intentional failure: FailingClass010_Test008");
+    void failingTest08_class010() {
+        fail("Intentional failure: FailingClass010_Test08");
     }
 
     @Test
-    void failingTest009_class010() {
-        fail("Intentional failure: FailingClass010_Test009");
+    void failingTest09_class010() {
+        fail("Intentional failure: FailingClass010_Test09");
     }
 
     @Test
-    void failingTest010_class010() {
-        fail("Intentional failure: FailingClass010_Test010");
+    void failingTest10_class010() {
+        fail("Intentional failure: FailingClass010_Test10");
+    }
+
+    @Test
+    void failingTest11_class010() {
+        fail("Intentional failure: FailingClass010_Test11");
+    }
+
+    @Test
+    void failingTest12_class010() {
+        fail("Intentional failure: FailingClass010_Test12");
+    }
+
+    @Test
+    void failingTest13_class010() {
+        fail("Intentional failure: FailingClass010_Test13");
+    }
+
+    @Test
+    void failingTest14_class010() {
+        fail("Intentional failure: FailingClass010_Test14");
+    }
+
+    @Test
+    void failingTest15_class010() {
+        fail("Intentional failure: FailingClass010_Test15");
+    }
+
+    @Test
+    void failingTest16_class010() {
+        fail("Intentional failure: FailingClass010_Test16");
+    }
+
+    @Test
+    void failingTest17_class010() {
+        fail("Intentional failure: FailingClass010_Test17");
+    }
+
+    @Test
+    void failingTest18_class010() {
+        fail("Intentional failure: FailingClass010_Test18");
+    }
+
+    @Test
+    void failingTest19_class010() {
+        fail("Intentional failure: FailingClass010_Test19");
+    }
+
+    @Test
+    void failingTest20_class010() {
+        fail("Intentional failure: FailingClass010_Test20");
+    }
+
+    @Test
+    void failingTest21_class010() {
+        fail("Intentional failure: FailingClass010_Test21");
+    }
+
+    @Test
+    void failingTest22_class010() {
+        fail("Intentional failure: FailingClass010_Test22");
     }
 }
